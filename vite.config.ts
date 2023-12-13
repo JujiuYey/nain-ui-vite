5e8584a20b92b9aabe4a952a89bbf1ebbc1d9061
jujiuYey <1041556532@qq.com>
2023-12-13 20:06:54 +0800

    test build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
+
+const rollupOptions = {
+
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
 
 export default defineConfig({
 
@@ -12,4 +21,17 @@ export default defineConfig({
       // options are passed on to @vue/babel-plugin-jsx
     })
   ],
+
+   // 添加库模式配置
+   build: {
+    rollupOptions,
+    minify:false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "NainUI",
+      fileName: "nain-ui",
+      // 导出模块格式
+      formats: ["es", "umd","iife"],
+    },
+  },
 });
